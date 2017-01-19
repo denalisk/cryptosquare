740745e26518019a984f7af79a8cd969445330c6
Derek Villars and Sam Kirsch <sam@>
2017-01-19 14:33:49 -0800

    add refactor for all functions in to encryptor

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,32 @@
-var characterStripper = function(textInput) {
-
+var encryptor = function(textInput) {
   var cleanText = textInput.replace(/[^a-z,0-9]/gi, '');
-
-  console.log(cleanText);
-  return cleanText;
-}
-
-var nearestSquare = function(number) {
+  var arrayInput = cleanText.split("");
+  var number = arrayInput.length;
   nearSquare = Math.ceil(Math.sqrt(number));
-  return nearSquare;
+  var sentence = [];
+  var word = [];
+  var counter = 0;
+  for (sentenceIndex = 0; sentenceIndex < number; sentenceIndex++) {
+    if (counter < nearSquare) {
+      word.push(arrayInput[sentenceIndex]);
+      counter += 1;
+    } else {
+      sentence.push(word);
+      word = [];
+      word.push(arrayInput[sentenceIndex])
+      counter = 1;
+    }
+  }
+  sentence.push(word);
+  console.log(sentence);
+  return sentence;
 };
-
 
 $(function(){
   $("form.crypto-form").submit(function(event) {
     event.preventDefault();
     var textInput = $("#text-input").val();
-    var textOutput = nearestSquare(characterStripper(textInput).length);
+    var textOutput = encryptor(textInput);
 
     $(".result").text(textOutput);
   })
